c7a04dfe68473b60d53f84c7d44259feaa83f769
Gerardo Barzola <gbarzolas@pucp.pe>
2016-01-13 16:14:59 +0000

    Version 1.1

--- demo_encuestas/app/controllers/answers_controller.rb
+++
@@ -1,9 +0,0 @@
-class AnswersController < ApplicationController
-
-private
-
-def answer_params
-      params.require(:answer).permit(:question_id, :content)
-end
-
-end
---
+++ encuesta/app/controllers/surveys_controller.rb
@@ -0,0 +1,52 @@
+class SurveysController < ApplicationController
+  def index
+    @surveys = Survey.all
+  end
+
+  def show
+    @survey = Survey.find(params[:id])
+  end
+
+  def new
+    @survey = Survey.new
+    3.times do 
+      q = @survey.questions.build
+      4.times { q.answers.build }
+    end
+  end
+
+  def create
+    @survey = Survey.new(survey_params)
+    if @survey.save
+      redirect_to @survey, notice: "Successfully created survey."
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @survey = Survey.find(params[:id])
+  end
+
+  def update
+    @survey = Survey.find(params[:id])
+    if @survey.update(params[:survey].permit(:name, questions_attributes: [:id, :survey_id, :content, :_destroy, answers_attributes: [:id, :question_id, :content, :_destroy ] ]))
+      redirect_to @survey, notice: "Successfully updated survey."
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @survey = Survey.find(params[:id])
+    @survey.destroy
+    redirect_to surveys_url, notice: "Successfully destroyed survey."
+  end
+  
+  private
+  
+  def survey_params  
+  params.require(:survey).permit(:name, questions_attributes: [:id, :survey_id, :content, :_destroy, answers_attributes: [:id, :question_id, :content, :_destroy ] ] )
+  end
+
+end
